9da8fc437aa18ce4cf58bb619db7b207a6f6cb75
abusalehtusar <tusargfx@gmail.com>
2026-02-02 00:26:02 +0600

    feat: Hello World with JetBrains Mono

--- app/src/main/java/dev/abu/material3/MainActivity.kt
+++ app/src/main/java/dev/abu/material3/MainActivity.kt
@@ -3,14 +3,16 @@ package dev.abu.material3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,17 +23,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Material 3")
+                    Greeting()
                 }
             }
         }
     }
 }
 
+val jetbrainsMono = FontFamily(
+    Font(R.font.jetbrains_mono_regular)
+)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello World",
+            fontFamily = jetbrainsMono,
+            style = MaterialTheme.typography.displayLarge
+        )
+    }
 }
