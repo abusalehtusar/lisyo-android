d518447bce4e384f392425448d454221e11954c3
abusalehtusar <tusargfx@gmail.com>
2026-02-02 10:50:13 +0600

    Fix misplaced import in Theme.kt

--- app/src/main/java/dev/abu/material3/ui/theme/Theme.kt
+++ app/src/main/java/dev/abu/material3/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -25,8 +26,6 @@ private val OceanColorScheme = darkColorScheme(
     onBackground = OnOceanDark,
     onSurface = OnOceanDark,
 )
-
-import androidx.compose.ui.graphics.Color
 
 @Composable
 fun LisyoTheme(
